1b145afc0cd042aeb91ffd28d08248598dbc0de4
yar075off <yaroslav2002vas@gmail.com>
2024-07-01 15:24:45 +0300

    user info add

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,16 @@ function App() {
 
   return (
     <div className="App">
+      <div>
+        Hello, {tg.initDataUnsafe?.user?.first_name}{' '}
+        {tg.initDataUnsafe?.user?.last_name}
+      </div>
+      <div>
+        Your username: {tg.initDataUnsafe?.user?.username}
+      </div>
+      <div>
+       <img src={tg.initDataUnsafe?.user?.photo_url} alt='photo'/>
+      </div>
       <button onClick={onClose}>close</button>
     </div>
   );
